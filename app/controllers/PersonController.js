5fd860d6d1ed4bacc981f11ae884efc07dcca872
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-08-03 17:03:37 -0500

    complete all

--- app/controllers/PersonController.js
+++ app/controllers/PersonController.js
@@ -24,3 +24,50 @@ PersonController.find = async (req, res) => {
         res.status(500).send('error')
     }
 }
+
+PersonController.edit = async (req, res) => {
+    try {
+        const { params: { id }, body } = req;
+        const person = await PersonService.edit(id, body);
+
+        return res.send(person);
+    }catch(error){
+        console.log(error);
+        res.status(500).send('error')
+    }
+}
+
+PersonController.delete = async (req, res) => {
+    try {
+        const { params: { id } } = req;
+        const person = await PersonService.delete(id);
+
+        return res.send(person);
+    }catch(error){
+        console.log(error);
+        res.status(500).send('error')
+    }
+}
+
+PersonController.findByName = async (req, res) => {
+    try {
+        const { query: { name } } = req;
+        const person = await PersonService.findByName(name);
+
+        return res.send(person);
+    }catch(error){
+        console.log(error);
+        res.status(500).send('error')
+    }
+}
+
+PersonController.listAll = async (req, res) => {
+    try {
+        const person = await PersonService.listAll();
+
+        return res.send(person);
+    }catch(error){
+        console.log(error);
+        res.status(500).send('error')
+    }
+}
