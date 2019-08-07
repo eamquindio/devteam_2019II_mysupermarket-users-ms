66f9b34f203018f8525ba49c66ba802d18178e70
Nicolas Davila <nikojako99@gmail.com>
2019-08-07 15:38:18 -0500

    [resolves #17] functionality created: delete users

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -1,4 +1,5 @@
 const UserController = module.exports;
+<<<<<<< HEAD
 const UserService = require('../services/UsersService');
 
 UserController.edit = async (req, res) => {
@@ -6,11 +7,23 @@ UserController.edit = async (req, res) => {
         const { params: { id }, body } = req;
         const user = await UserService.edit(id, body);
         return res.send();
+=======
+const UserService = require('../services/UserService');
+
+
+UserController.delete = async (req, res) => {
+    try {
+        const { params: { id } } = req;
+        const user = await UserService.delete(id);
+
+        return res.status(200).json({'success' : user});
+>>>>>>> [resolves #17] functionality created: delete users
     }catch(error){
         console.log(error);
         res.status(500).send('error')
     }
 }
+<<<<<<< HEAD
 
 UserController.save = async (req, res) => {
     const { body } = req;
@@ -22,3 +35,5 @@ UserController.save = async (req, res) => {
         res.status(500).send('error');
     }
 }
+=======
+>>>>>>> [resolves #17] functionality created: delete users
--- app/repositories/UserRepository.js
+++ app/repositories/UserRepository.js
@@ -1,5 +1,9 @@
 const UserRepository = module.exports;
 const DB = require('../utils/DB');
 
+<<<<<<< HEAD
 UserRepository.edit = (userId, user) => DB('users').update(user).where({ userId });
 UserRepository.create = (user) => DB('users').insert(user);
+=======
+UserRepository.delete = (user_id) => DB('users').del().where({ user_id });
+>>>>>>> [resolves #17] functionality created: delete users
