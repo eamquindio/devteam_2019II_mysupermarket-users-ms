0603dc81bac0061f601cc980c27e1cd973750e9e
Alvaro Corrales <alvarito1050@gmail.com>
2019-08-08 10:56:58 -0500

    [resolves #14] Find user by name routes

--- app/repositories/UserRepository.js
+++ app/repositories/UserRepository.js
@@ -3,8 +3,5 @@ const DB = require('../utils/DB');
 
 UserRepository.init = () => console.log(DB);
 
-<<<<<<< HEAD
 UserRepository.create = user => DB('users').insert(user);
-=======
->>>>>>> [resolves #14] Correction find by user name
 UserRepository.findByName = userName => DB('users').select('*').where({ userName });
--- app/routes.js
+++ app/routes.js
@@ -15,10 +15,20 @@ router.get('/persons/all', PersonController.listAll);
 
 // Users Routes
 <<<<<<< HEAD
+<<<<<<< HEAD
 router.post('/users', UserController.save); // Create new users
 // Users routes
 =======
 >>>>>>> [resolves #14] Correction find by user name
+=======
+>>>>>>> [resolves #14] Find user by name routes
 router.get('/users/find_by_name/:name', UserController.findByName);
+=======
+router.put('/users/:id', UserController.edit); // edit user
+// Users routes
+
+router.get('/users/find_by_name', UserController.findByName);
+
+>>>>>>> [resolves #14] Find user by name routes
 
 module.exports = router;
