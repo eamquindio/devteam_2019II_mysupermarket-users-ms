d95e0802a2075c7b812e677917e7030dfde69d32
Jhon Quirama <quirama80@gmail.com>
2019-08-10 10:03:41 -0500

    file modified

--- app/services/UsersService.js
+++ app/services/UsersService.js
@@ -1,11 +1,10 @@
 const UserService = module.exports;
 const UserRepository = require('../repositories/UserRepository');
 
-//test 2....
 
-UserService.listAll = (name) => {
 UserService.listAll = () => {
     console.log('findByName user')
+
     return UserRepository.listAll();
 }
 
