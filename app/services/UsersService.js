52d90968e392c77c7d82a01cc272155dcb490bb1
Jhon Quirama <quirama80@gmail.com>
2019-08-20 16:28:55 -0500

    [resolves #16] merge require

--- app/services/UsersService.js
+++ app/services/UsersService.js
@@ -1,8 +1,6 @@
 const UsersService = module.exports;
 const UserRepository = require('../repositories/UserRepository');
 
-UsersService.init = () => console.log(UserRepository);
-
 UsersService.create = (user) => {
   console.log('creating user');
 
