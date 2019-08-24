4c4bdfd54d28c03a7c782957c12c3a82518749d7
Daryl Ospina <daryl.8220@gmail.com>
2019-08-24 10:07:43 -0500

    resolve name of variable

--- app/repositories/UserRepository.js
+++ app/repositories/UserRepository.js
@@ -3,7 +3,9 @@ const DB = require('../utils/DB');
 
 UserRepository.create = user => DB('users').insert(user).returning('*');
 UserRepository.findByName = userName => DB('users').select('*').where({ userName });
-UserRepository.find = id => DB('users').select('*').where({ id }).first();
+// eslint-disable-next-line linebreak-style
+// eslint-disable-next-line camelcase
+UserRepository.find = user_id => DB('users').select('*').where({ user_id }).first();
 UserRepository.delete = userId => DB('users').del().where({ userId }).returning('*');
 UserRepository.listAll = () => DB('users').select('*');
 UserRepository.edit = (userId, user) => DB('users').update(user).where({ userId }).returning('*');
