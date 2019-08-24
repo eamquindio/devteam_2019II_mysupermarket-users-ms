9475519931fd453febcddc0ca835eb2f1c26fb40
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-08-24 09:34:55 -0500

    fix user repo

--- app/repositories/UserRepository.js
+++ app/repositories/UserRepository.js
@@ -1,9 +1,9 @@
 const UserRepository = module.exports;
 const DB = require('../utils/DB');
 
-UserRepository.create = user => DB('users').insert(user);
+UserRepository.create = user => DB('users').insert(user).returning('*');
 UserRepository.findByName = userName => DB('users').select('*').where({ userName });
-UserRepository.find = id => DB('users').select('*').where({ id });
-UserRepository.delete = userId => DB('users').del().where({ userId });
+UserRepository.find = id => DB('users').select('*').where({ id }).first();
+UserRepository.delete = userId => DB('users').del().where({ userId }).returning('*');
 UserRepository.listAll = () => DB('users').select('*');
-UserRepository.edit = (userId, user) => DB('users').update(user).where({ userId });
+UserRepository.edit = (userId, user) => DB('users').update(user).where({ userId }).returning('*');
